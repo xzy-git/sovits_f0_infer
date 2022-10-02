e0687c2f297c41f1a1bf811921e1e08ab622fa6a
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-10-02 15:22:54 +0800

    update

--- infer_tool.py
+++ infer_tool.py
@@ -1,9 +1,50 @@
 import os
-import torch
-import soundfile
-import torchaudio
+
 import numpy as np
+import soundfile
+import torch
+import torchaudio
 from pydub import AudioSegment
+
+dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_units(path, hubert_soft):
+    source, sr = torchaudio.load(path)
+    source = torchaudio.functional.resample(source, sr, 16000)
+    source = source.unsqueeze(0).to(dev)
+    with torch.inference_mode():
+        units = hubert_soft.units(source)
+        return units
+
+
+def transcribe(path, length, transform, feature_input):
+    feature_pit = feature_input.compute_f0(path)
+    feature_pit = feature_pit * 2 ** (transform / 12)
+    feature_pit = resize2d_f0(feature_pit, length)
+    coarse_pit = feature_input.coarse_f0(feature_pit)
+    return coarse_pit
+
+
+def infer(file_name, speaker_id, tran, target_sample, net_g_ms, hubert_soft, feature_input):
+    source_path = "./wav_temp/input/" + file_name
+    audio, sample_rate = torchaudio.load(source_path)
+    input_size = audio.shape[-1]
+
+    sid = torch.LongTensor([int(speaker_id)]).to(dev)
+    soft = get_units(source_path, hubert_soft).squeeze(0).cpu().numpy()
+    pitch = transcribe(source_path, soft.shape[0], tran, feature_input)
+    pitch = torch.LongTensor(pitch).unsqueeze(0).to(dev)
+    stn_tst = torch.FloatTensor(soft)
+    with torch.no_grad():
+        x_tst = stn_tst.unsqueeze(0).to(dev)
+        x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).to(dev)
+        audio = \
+            net_g_ms.infer(x_tst, x_tst_lengths, pitch, sid=sid, noise_scale=.3, noise_scale_w=0.5,
+                           length_scale=1)[0][
+                0, 0].data.float().cpu().numpy()
+    soundfile.write("./wav_temp/output/" + file_name, audio,
+                    int(audio.shape[0] / input_size * target_sample))
 
 
 def resize2d_f0(x, target_len):
@@ -32,13 +73,13 @@ def cut(c_time, file_path, vocal_name, out_dir):
     audio_segment[total * c_time * 1000:].export(f"{out_dir}/{vocal_name}-{total}.wav", format="wav")  # 缺少结尾的音频片段
 
 
-def resample_to_22050(audio_path):
+def wav_resample(audio_path, tar_sample):
     raw_audio, raw_sample_rate = torchaudio.load(audio_path)
-    audio_22050 = torchaudio.transforms.Resample(orig_freq=raw_sample_rate, new_freq=22050)(raw_audio)[0]
-    soundfile.write(audio_path, audio_22050, 22050)
+    audio_22050 = torchaudio.transforms.Resample(orig_freq=raw_sample_rate, new_freq=tar_sample)(raw_audio)[0]
+    soundfile.write(audio_path, audio_22050, tar_sample)
 
 
 def fill_a_to_b(a, b):
     if len(a) < len(b):
-        for i in range(0, len(b) - len(a)):
+        for _ in range(0, len(b) - len(a)):
             a.append(a[0])
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import shutil
 
 import demjson
-import soundfile
 import torch
 import torchaudio
 
@@ -16,38 +15,20 @@ from wav_temp import merge
 
 logging.getLogger('numba').setLevel(logging.WARNING)
 
-
-def get_units(path):
-    source, sr = torchaudio.load(path)
-    source = torchaudio.functional.resample(source, sr, 16000)
-    source = source.unsqueeze(0).to(dev)
-    with torch.inference_mode():
-        units = hubert_soft.units(source)
-        return units
-
-
-def transcribe(path, length, transform):
-    feature_pit = featureInput.compute_f0(path)
-    feature_pit = feature_pit * 2 ** (transform / 12)
-    feature_pit = infer_tool.resize2d_f0(feature_pit, length)
-    coarse_pit = featureInput.coarse_f0(feature_pit)
-    return coarse_pit
-
-
 # 自行创建pth文件夹，放置hubert、sovits模型，创建raw、results文件夹
 # 可填写音源文件列表，音源文件格式为单声道22050采样率wav，放置于raw文件夹下
 clean_names = ["多声线测试"]
 # bgm、trans分别对应歌曲列表，若能找到相应文件、则自动合并伴奏，若找不到bgm，则输出干声（不使用bgm合成多首歌时，可只随意填写一个不存在的bgm名）
 bgm_names = ["bgm1"]
 # 合成多少歌曲时，若半音数量不足、自动补齐相同数量（按第一首歌的半音）
-trans = [-3]  # 加减半音数（可为正负）s
+trans = [0]  # 加减半音数（可为正负）s
 # 每首歌同时输出的speaker_id
-id_list = [2]
+id_list = [0]
 
 # 每次合成长度，建议30s内，太高了爆掉显存(gtx1066一次15s以内）
-cut_time = 60
-model_name = "530_epochs"  # 模型名称（pth文件夹下）
-config_name = "yilanqiu.json"  # 模型配置（config文件夹下）
+cut_time = 30
+model_name = "101_epochs"  # 模型名称（pth文件夹下）
+config_name = "sovits_pre.json"  # 模型配置（config文件夹下）
 
 # 自行下载hubert-soft-0d54a1f4.pt改名为hubert.pt放置于pth文件夹下
 # https://github.com/bshall/hubert/releases/tag/v0.1
@@ -66,12 +47,13 @@ net_g_ms = SynthesizerTrn(
 _ = utils.load_checkpoint(f"pth/{model_name}.pth", net_g_ms, None)
 _ = net_g_ms.eval().to(dev)
 
-featureInput = FeatureInput(hps_ms.data.sampling_rate, hps_ms.data.hop_length)
+target_sample = hps_ms.data.sampling_rate
+feature_input = FeatureInput(hps_ms.data.sampling_rate, hps_ms.data.hop_length)
 # 自动补齐
 infer_tool.fill_a_to_b(bgm_names, clean_names)
 infer_tool.fill_a_to_b(trans, clean_names)
 for clean_name, bgm_name, tran in zip(clean_names, bgm_names, trans):
-    infer_tool.resample_to_22050(f'./raw/{clean_name}.wav')
+    infer_tool.wav_resample(f'./raw/{clean_name}.wav', target_sample)
     for speaker_id in id_list:
         speakers = demjson.decode_file(f"configs/{config_name}")["speakers"]
         out_audio_name = model_name + f"_{clean_name}_{speakers[speaker_id]}"
@@ -81,34 +63,18 @@ for clean_name, bgm_name, tran in zip(clean_names, bgm_names, trans):
 
         raw_audio_path = f"./raw/{clean_name}.wav"
         audio, sample_rate = torchaudio.load(raw_audio_path)
+        audio_time = audio.shape[-1] / target_sample
 
-        audio_time = audio.shape[-1] / 22050
+        # 源音频切割方案
         if audio_time > 1.3 * int(cut_time):
             infer_tool.cut(int(cut_time), raw_audio_path, out_audio_name, "./wav_temp/input")
         else:
             shutil.copy(f"./raw/{clean_name}.wav", f"./wav_temp/input/{out_audio_name}-0.wav")
-        file_list = os.listdir("./wav_temp/input")
 
         count = 0
+        file_list = os.listdir("./wav_temp/input")
         for file_name in file_list:
-            source_path = "./wav_temp/input/" + file_name
-            audio, sample_rate = torchaudio.load(source_path)
-            input_size = audio.shape[-1]
-
-            sid = torch.LongTensor([int(speaker_id)]).to(dev)
-            soft = get_units(source_path).squeeze(0).cpu().numpy()
-            pitch = transcribe(source_path, soft.shape[0], tran)
-            pitch = torch.LongTensor(pitch).unsqueeze(0).to(dev)
-            stn_tst = torch.FloatTensor(soft)
-            with torch.no_grad():
-                x_tst = stn_tst.unsqueeze(0).to(dev)
-                x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).to(dev)
-                audio = \
-                    net_g_ms.infer(x_tst, x_tst_lengths, pitch, sid=sid, noise_scale=.3, noise_scale_w=0.5,
-                                   length_scale=1)[0][
-                        0, 0].data.float().cpu().numpy()
-
-            soundfile.write("./wav_temp/output/" + file_name, audio, int(audio.shape[0] / input_size * 22050))
+            infer_tool.infer(file_name, speaker_id, tran, target_sample, net_g_ms, hubert_soft, feature_input)
             count += 1
             print("%s success: %.2f%%" % (file_name, 100 * count / len(file_list)))
         merge.run(out_audio_name, bgm_name, out_audio_name)
