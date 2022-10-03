e1abc6e9355588c35d2c4996d55a7cbd50efcacf
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-10-03 11:42:56 +0800

    update

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from wav_temp import merge
 
 # 自行创建pth文件夹，放置hubert、sovits模型，创建raw、results文件夹
 # 可填写音源文件列表，音源文件格式为wav，放置于raw文件夹下
-clean_names = ["十年"]
+clean_names = ["像我这样的人", "嘘月"]
 # bgm、trans分别对应歌曲列表，若能找到相应文件、则自动合并伴奏，若找不到bgm，则输出干声（不使用bgm合成多首歌时，可只随意填写一个不存在的bgm名）
 bgm_names = ["bgm1"]
 # 合成多少歌曲时，若半音数量不足、自动补齐相同数量（按第一首歌的半音）
-trans = [0]  # 加减半音数（可为正负）s
+trans = [3]  # 加减半音数（可为正负）s
 # 每首歌同时输出的speaker_id
-id_list = [0]
+id_list = [3]
 
 # 每次合成长度，建议30s内，太高了爆显存(gtx1066一次30s以内）
 cut_time = 20
@@ -59,10 +59,8 @@ for clean_name, bgm_name, tran in zip(clean_names, bgm_names, trans):
         file_list = os.listdir("./wav_temp/input")
         for file_name in file_list:
             source_path = "./wav_temp/input/" + file_name
-            input_size = audio.shape[-1]
             out_audio, out_sr = infer_tool.infer(source_path, speaker_id, tran)
-            soundfile.write("./wav_temp/output/" + file_name, out_audio,
-                            int(out_sr / input_size * target_sample))
+            soundfile.write("./wav_temp/output/" + file_name, out_audio, target_sample)
             count += 1
             print("%s success: %.2f%%" % (file_name, 100 * count / len(file_list)))
         merge.run(out_audio_name, bgm_name, out_audio_name)
