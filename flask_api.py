da5381d7772c613452b6cf8e5b9cbad4160a055b
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-10-12 22:23:10 +0800

    update

--- flask_api.py
+++ flask_api.py
@@ -25,16 +25,16 @@ def voice_change_model():
     request_form = request.form
     wave_file = request.files.get("sample", None)
     # 变调信息
-    f_pitch_change = int(float(request_form.get("fPitchChange", 0)))
+    f_pitch_change = float(request_form.get("fPitchChange", 0))
     raw_sample = int(float(request_form.get("sampleRate", 0)))
+    speak_id = int(float(request_form.get("sSpeakId", 0)))
     raw_wav_path = f"{http_temp_path}/http_in.wav"
-    print(raw_sample, f_pitch_change)
     # http获得wav文件并转换
     w_file = wave_file.read()
     with open(raw_wav_path, 'wb') as fop:
         fop.write(w_file)
     infer_tool.format_wav(raw_wav_path, target_sample)
-    out_audio, out_sr = infer_tool.infer(raw_wav_path, speaker_id, f_pitch_change, net_g_ms, hubert_soft,
+    out_audio, out_sr = infer_tool.infer(raw_wav_path, speak_id, f_pitch_change, net_g_ms, hubert_soft,
                                          feature_input)
     out_wav_path = f"{http_temp_path}/http_out.wav"
     soundfile.write(out_wav_path, out_audio, target_sample)
@@ -43,8 +43,6 @@ def voice_change_model():
 
 
 if __name__ == '__main__':
-    # 这个是人物音色id，发布模型的时候、发布人应注明
-    speaker_id = 4
     # 每个模型和config是唯一对应的
     model_name = "152_epochs.pth"
     config_name = "nyarumul.json"
